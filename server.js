8a8409473637050e0a33a9ce2fd1236383ba0040
samadsammu <112466499+samadsammu@users.noreply.github.com>
2026-02-05 12:55:18 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+app.use('/api/products', productRoutes);
+app.use('/api/users', userRoutes);
+
+// MongoDB Connection
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
+// Basic Route
+app.get('/', (req, res) => {
+    res.send('Zaygha Designs API is running...');
+});
+
+// Start Server
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
